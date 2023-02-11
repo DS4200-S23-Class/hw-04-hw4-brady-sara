c325053fea00b5c6507a6e2a430075d91e187c89
Brady Hobson <bradyhobson250@gmail.com>
2023-02-11 17:01:58 -0500

    working on js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 // JS file hw4
 // Sara & Brady
 
+// Doesnt work yet :(
+function pointClicked() {
+	console.log("clicked");
+	let pointed = document.getElementById("point");
+
+	pointed.classList.toggle("addBorder");
+
+
+}
+
+
+document.getElementsById("point")
+		.addEventListener('click', pointClicked);
