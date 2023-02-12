bf933110cbdcfa295b9f951d8404c94fa75818d5
Sara Kent <kent.sa@northeastern.edu>
2023-02-12 16:10:28 -0500

    user added points implemented

--- main.js
+++ main.js
@@ -19,6 +19,33 @@ function pointClicked() {
 	document.getElementById("last_point2").innerHTML = text2;
 }
 
+
+function addPoint() {
+	// get the user input selections
+	let xInput = document.getElementById("x-coord");
+	let yInput = document.getElementById("y-coord");
+
+	// get the values from the user selections
+	let xCoord = xInput.value;
+	let yCoord = yInput.value;
+
+	// convert the values to the same ratio for the graph
+	let x = xCoord * 50;
+	let y = 500 - (yCoord * 50);
+
+	// get frame to add a point to graph
+	let frame = document.getElementById("frame");
+
+	// add a circle to the html
+	let circleStr = '<circle cx="' + x + '" cy="' + y + '" r="10"/>';
+	frame.innerHTML += circleStr;
+}
+
+// get the button Id
+let pointButton =  document.getElementById("subButton");
+// listen for a click
+pointButton.addEventListener("click", addPoint);
+
 // Create list of points
 let points = document.getElementsByTagName("circle");
 
