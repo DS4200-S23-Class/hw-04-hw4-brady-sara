8f2ffd716fbfed4e5f52f136312fca24a03f26b5
Brady Hobson <bradyhobson250@gmail.com>
2023-02-11 19:16:23 -0500

    updated text

--- main.js
+++ main.js
@@ -5,6 +5,18 @@
 function pointClicked() {
 	// change class if clicked based on event listener
 	this.classList.toggle("addBorder");
+
+	// get x and y coordinates
+	let x_var = this.getAttribute("cx") / 50;
+	let y_var = (500 - this.getAttribute("cy")) / 50;
+
+	// create new text
+	let text1 = "Last point clicked: "
+	let text2 = "(" + x_var +"," + y_var + ")"
+	
+	// replace text
+	document.getElementById("last_point1").innerHTML = text1;
+	document.getElementById("last_point2").innerHTML = text2;
 }
 
 // Create list of points
